1728516b058e123d65ba3de8addb3e8bc99353b3
Nguyen Thanh Quan <73626660+QuanBlue@users.noreply.github.com>
2022-02-25 11:16:15 +0700

    update program
    - Fix `miller_rabin(...)` 
    - Find issue `power_mod(...)` fail in some test - Need fix: @DuckyMomo20012

--- 19127392_19127525_19127625_19127631.cpp
+++ 19127392_19127525_19127625_19127631.cpp
@@ -5,91 +5,101 @@ using namespace std;
 
 long power_mod(long b, long e, long m)
 {
-   // input: b (base), e (exponent), m (modulus)
-   // output: b^e mod m
+    // input: b (base), e (exponent), m (modulus)
+    // output: b^e mod m
 
-   b %= m;
-   long result = 1;
-   while (e > 0)
-   {
-      // Check odds
-      if (e & 1)
-      {
-         result = result * b % m;
-      }
-      b = b * b % m;
-      e >>= 1;
-   }
+    // case wrong:
+    // b = 20642
+    // e = 649913
+    // m = 1299827
+    // expect: 1299826 - calc by: https://www.mtholyoke.edu/courses/quenell/s2003/ma139/js/powermod.html
+    // result: 1078532
 
-   return result;
+    b %= m;
+    long result = 1;
+    while (e > 0)
+    {
+        // Check odds
+        if (e & 1)
+        {
+            result = result * b % m;
+        }
+        b = b * b % m;
+        e >>= 1;
+    }
+
+    return result;
 }
 
-void parse_n(int n, int &k, int &m)
+void parse_n(int n, int& k, int& m)
 {
-   n = n - 1;
+    n = n - 1;
 
-   // while n even
-   while (n % 2 == 0)
-   {
-      k++;
-      n = n / 2;
-   }
-   m = n;
+    // while n even
+    while (n % 2 == 0)
+    {
+        k++;
+        n = n / 2;
+    }
+    m = n;
 }
 
 bool miller_rabin(long n)
 {
-   // input: n
-   // output: true/false
-   int k = 0, m = 0;
+    // input: n
+    // output: true/false
 
-   // parse n = 2^k*m + 1
-   parse_n(n, k, m);
+    // check if n even or = 2
+    if (n == 2)
+        return true;
+    else if (n < 2 || n % 2 == 0)
+        return false;
 
-   // pick random a (1, n-1)
-   int a = rand() % (n - 1) + 1;
-   cout << "a:" << a << endl;
-   long b = power_mod(a, m, n);
-   cout << "b:" << b << endl;
+    int k = 0, m = 0;
 
-   if (b % n == 1)
-      return true;
+    // parse n = 2^k*m + 1
+    parse_n(n, k, m);
 
-   for (int i = 0; i < k; i++)
-   {
-      if (b % n == n - 1)
-         return true;
-      b = power_mod(b, 2, n);
-   }
+    // pick random a (1, n-1)
+    long a = rand() % (n - 1) + 1;
+    long b = power_mod(a, m, n);
+    
+    if (b % n == 1)
+        return true;
 
-   return false;
+    for (int i = 0; i < k; i++)
+    {
+        if (b % n == n - 1)
+            return true;
+        b = power_mod(b, 2, n);
+    }
+
+    return false;
 }
 
 int main()
 {
-   srand(time(NULL));
+    srand(time(NULL));
 
-   long n;
-   cout << "-------------------------------------------------" << endl;
-   cout << "--                 Miller Rabin                --" << endl;
-   cout << "--         ---------------------------         --" << endl;
-   cout << "--  19127392 - 19127525 - 19127625 - 19127631  --" << endl;
-   cout << "-------------------------------------------------" << endl;
+    long n;
+    cout << "-------------------------------------------------" << endl;
+    cout << "--                 Miller Rabin                --" << endl;
+    cout << "--         ---------------------------         --" << endl;
+    cout << "--  19127392 - 19127525 - 19127625 - 19127631  --" << endl;
+    cout << "-------------------------------------------------" << endl;
 
-   // cout << "> Input n:";
-   // cin >> n;
+    /*while (true) {
+        cout << "\n> Input n:";
+        cin >> n;
 
-   // bool ML = miller_rabin(n);
-   // if (ML == true)
-   //    cout << n << " is prime";
-   // else
-   //    cout << n << " is not prime";
-   for (int i = 0; i < 1000; i++)
-   {
-      cout << i << ": " << miller_rabin(i) << endl;
-      if (i % 100 == 0)
-         cout << "-------------" << endl;
-   }
-
-   return 0;
+        bool ML = miller_rabin(n);
+        if (ML == true)
+            cout << n << " is prime";
+        else
+            cout << n << " is not prime";
+    }*/
+   
+    cout << power_mod(20642, 649913, 1299827)<<endl;
+   
+    return 0;
 }
