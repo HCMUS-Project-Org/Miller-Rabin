38424f60ad7701640212b82a2db555ff58be03b3
19127631_Dương Tiến Vinh <64480713+DuckyMomo20012@users.noreply.github.com>
2022-02-26 08:51:55 +0700

    Fix infinity loop
    - Flush cin after input
    - Change data type to long long

--- 19127392_19127525_19127625_19127631.cpp
+++ 19127392_19127525_19127625_19127631.cpp
@@ -5,102 +5,104 @@ using namespace std;
 
 long power_mod(long long b, long e, long m)
 {
-    // input: b (base), e (exponent), m (modulus)
-    // output: b^e mod m
+   // input: b (base), e (exponent), m (modulus)
+   // output: b^e mod m
 
-    // case wrong:
-    // b = 20642
-    // e = 649913
-    // m = 1299827
-    // expect: 1299826 - calc by: https://www.mtholyoke.edu/courses/quenell/s2003/ma139/js/powermod.html
-    // result: 1078532
+   b %= m;
+   long result = 1;
+   while (e > 0)
+   {
+      // Check odds
+      if (e & 1)
+      {
+         result = result * b % m;
+      }
+      // b overflew before calc modular, so use long long for base.
+      b = b * b % m;
+      e >>= 1;
+   }
 
-    b %= m;
-    long result = 1;
-    while (e > 0)
-    {
-        // Check odds
-        if (e & 1)
-        {
-            result = result * b % m;
-        }
-        // b overflew before calc modular, so use long long for base.
-        b = b * b % m;
-        e >>= 1;
-    }
-
-    return result;
+   return result;
 }
 
-void parse_n(int n, int& k, int& m)
+void parse_n(long long n, int &k, long long &m)
 {
-    n = n - 1;
+   n = n - 1;
 
-    // while n even
-    while (n % 2 == 0)
-    {
-        k++;
-        n = n / 2;
-    }
-    m = n;
+   // while n even
+   while (n % 2 == 0)
+   {
+      k++;
+      n = n / 2;
+   }
+   m = n;
 }
 
-bool miller_rabin(long n)
+bool miller_rabin(long long n)
 {
-    // input: n
-    // output: true/false
+   // input: n
+   // output: true/false
 
-    // check if n even or = 2
-    if (n == 2)
-        return true;
-    else if (n < 2 || n % 2 == 0)
-        return false;
+   // check if n even or = 2
+   if (n == 2)
+      return true;
+   else if (n < 2 || n % 2 == 0)
+      return false;
 
-    int k = 0, m = 0;
+   int k = 0;
+   long long m = 0;
 
-    // parse n = 2^k*m + 1
-    parse_n(n, k, m);
+   // parse n = 2^k*m + 1
+   parse_n(n, k, m);
 
-    // pick random a (1, n-1)
-    long a = rand() % (n - 1) + 1;
-    long b = power_mod(a, m, n);
+   // pick random a (1, n-1)
+   long long a = rand() % (n - 1) + 1;
+   long long b = power_mod(a, m, n);
 
-    if (b % n == 1)
-        return true;
+   if (b % n == 1)
+      return true;
 
-    for (int i = 0; i < k; i++)
-    {
-        if (b % n == n - 1)
-            return true;
-        b = power_mod(b, 2, n);
-    }
+   for (int i = 0; i < k; i++)
+   {
+      if (b % n == n - 1)
+         return true;
+      b = power_mod(b, 2, n);
+   }
 
-    return false;
+   return false;
 }
 
 int main()
 {
-    srand(time(NULL));
+   srand(time(NULL));
 
-    long n;
-    cout << "-------------------------------------------------" << endl;
-    cout << "--                 Miller Rabin                --" << endl;
-    cout << "--         ---------------------------         --" << endl;
-    cout << "--  19127392 - 19127525 - 19127625 - 19127631  --" << endl;
-    cout << "-------------------------------------------------" << endl;
+   long long n;
+   string input;
+   cout << "-------------------------------------------------" << endl;
+   cout << "--                 Miller Rabin                --" << endl;
+   cout << "--         ---------------------------         --" << endl;
+   cout << "--  19127392 - 19127525 - 19127625 - 19127631  --" << endl;
+   cout << "-------------------------------------------------" << endl;
 
-    /*while (true) {
-        cout << "\n> Input n:";
-        cin >> n;
+   while (true) {
+      cout << "\n> Input n:";
+      // Flush input to prevent infinity loop
+      cin >> n;
+      cin.clear();
+      fflush(stdin);
+      if (n < 0) {
+        cout << "Input number should be a positive one" << endl;
+        continue;
+      }
+      bool ML = miller_rabin(n);
+      if (ML == true)
+        // if the result is different from the input -> overflow
+        cout << n << " is prime";
+      else
+        cout << n << " is not prime";
 
-        bool ML = miller_rabin(n);
-        if (ML == true)
-            cout << n << " is prime";
-        else
-            cout << n << " is not prime";
-    }*/
+      cout << endl;
+   };
 
-    cout << power_mod(20642, 649913, 1299827)<<endl;
-
-    return 0;
+   return 0;
 }
